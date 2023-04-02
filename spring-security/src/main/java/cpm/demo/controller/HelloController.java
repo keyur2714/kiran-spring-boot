eecbc9019c56818080bc7275a436a2468c036267
keyur2714 <keyurjava27@gmail.com>
2023-04-02 17:54:26 +0530

    Added Spring Security

---
+++ spring-security/src/main/java/cpm/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cpm.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+	@GetMapping("/sayHello")
+	public String sayHello() {
+		return "Hello Radhe Krishna...!";
+	}
+	
+}
